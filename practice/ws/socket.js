e5d5e040e79be5c84c57647b6407e1b6a9f4e827
Hms200 <96504521+Hms200@users.noreply.github.com>
2022-07-11 23:32:45 +0900

    websocket 연결

---
+++ practice/ws/socket.js
@@ -0,0 +1,29 @@
+const WebSocket = require('ws');
+
+module.exports = (server) => {
+    const wss = new WebSocket.Server({ server });
+
+    wss.on('connection', (ws, req) => {
+        const ip = req.headers['x-forwarded-for'] ||
+                    req.connection.remoteAddress;
+        console.log('New client' + ip);
+        ws.on('message', (message) => {             // 클라이언트로부터 받은 메세지
+            console.log(message);
+        });
+
+        ws.on('error', (err) => {
+            console.error(err);
+        });
+
+        ws.on('close', () => {
+            console.log('접속 해제');
+            clearInterval(ws.interval);
+        });
+
+        ws.interval = setInterval(() => {      // 서버에서 보내는 메세지 3초마다.
+            if (ws.readyState === ws.OPEN){             // OPEN, CLOSE, CLOSINBG, CONNECTING
+                ws.send('Message from server')
+            }
+        }, 3000);
+    });
+}
