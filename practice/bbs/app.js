c6dd06301e533cc429dd94e880bd43b08c05ed25
Hms200 <96504521+Hms200@users.noreply.github.com>
2022-07-15 14:53:21 +0900

    게시판 만들기 model 작성

---
+++ practice/bbs/app.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const socket = require('socket.io');
+const dotenv = require('dotenv');
+const flash = require('connect-flash');
+
+
+const port = process.env.PORT || 3000;
+dotenv.config();
+const app = express();
+
+// routes
+const postRoutes = require('./routes/posts');
+const userRoutes = require('./routes/users');
+
+// model
+const User = require('./models/User');
+const Post = require('./models/Post')
+
+// 채팅 유저
+const onlineChatUser = {};
+
+// cookie, session, flash
+app.use(cookieParser(process.env.SECRET));
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(flash());
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// MongoDB Connection
+mongoose
+    .connect('mongodb://root:example@localhost:27017/roadbook?authSource=admin', {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('mongoDB connected');
+    })
+    .catch((err) => {
+        console.error(err)
+    });
+
+// 전역변수
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    res.locals.login = req.isAuthenticated();
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
+});
+
+// routers
+app.use('/', userRoutes);
+app.use('/', postRoutes);
+
+const server = app.listen(port, () => {
+    console.log(port + 'is listening...');
+});
+
+// websocket
+const io = socket(server);
+
+const room = io.of('/chat');
+room.on('connection', socket => {
+    console.log('new User : ', socket.id);
+    room.emit('newUser', { socketID: socket.id });
+
+    socket.on('newUser', data => {
+        if(!(data.name in onlineChatUser)) {
+            onlineChatUser[data.name] = data.socketID;
+            socket.name = data.name;
+            room.emit('updateUserList', Object.keys((onlineChatUser)));
+            console.log('Online users : ' + Object.keys(onlineChatUser));
+        }
+    });
+
+    socket.on('disconnect', () => {
+        delete onlineChatUser[socket.name];
+        room.emit('updateUserList', Object.keys(onlineChatUser));
+        console.log(`user ${ socket.name } is disconnected`);
+    });
+
+    socket.on('chat', data => {
+        console.log(data);
+        if(data.to === 'Global Chat') {
+            room.emit('chat', data);
+        } else if(data.to) {
+            room.to(onlineChatUser[data.name].emit('chat', data));
+            room.to(onlineChatUser[data.to].emit('chat', data));
+        }
+    });
+});
+
---
+++ practice/bbs/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+let UserSchema = new mongoose.Schema({
+    username: String,
+    firstName: String,
+    lastName: String,
+    password: String,
+    profile: String,
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Post'
+        }
+    ],
+    linked_posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Post'
+        }
+    ],
+    linked_comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Post'
+        }
+    ],
+    friends: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    friendRequests: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+});
+
+UserSchema.plugin(passportLocalMongoose);
+let User = mongoose.model('User', UserSchema);
+
+module.exports = User;
